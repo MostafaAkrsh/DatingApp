6ad7d1c84106159542778cb28653b82c17b5a99f
MostafaAkrsh <43042153+MostafaAkrsh@users.noreply.github.com>
2021-08-30 13:51:30 +0200

    registration / login / logout

---
+++ SPA/src/app/_services/account.service.ts
@@ -0,0 +1,55 @@
+import { User } from './../_models/user';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { ReplaySubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  baseUrl = 'https://localhost:44395/api/';
+
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+    login(model:any)
+    {
+      return this.http.post<User>(this.baseUrl + 'account/login' , model).pipe(
+        map(( response : User ) => {
+          const user = response;
+          if(user){
+            localStorage.setItem('user',JSON.stringify(user));
+            this.currentUserSource.next(user);
+          }
+        })
+      )
+    }
+
+    register(model:any){
+      return this.http.post(this.baseUrl + 'account/register',model).pipe(
+        map((User: any) => {
+          if(User) {
+            localStorage.setItem('user',JSON.stringify(User));
+            this.currentUserSource.next(User);
+          }
+          return User;
+        })
+      )
+    }
+
+
+    setCurrentUser(user:User){
+      this.currentUserSource.next(user);
+    }
+
+    logout() {
+      localStorage.removeItem('user');
+      this.currentUserSource.next();
+    }
+}
+
+
