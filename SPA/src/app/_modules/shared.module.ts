82bc35e523bff3e6dbf7139ae08adf3e101a66ff
MostafaAkrsh <43042153+MostafaAkrsh@users.noreply.github.com>
2021-12-15 13:37:24 +0200

    register form

--- SPA/src/app/_modules/shared.module.ts
+++ SPA/src/app/_modules/shared.module.ts
@@ -6,7 +6,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs'
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
-
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker'
 @NgModule({
   declarations: [],
   imports: [
